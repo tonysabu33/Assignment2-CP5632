ac119ad8ea8162b331be1fd239e8a352fa25f3a9
tonysabu33 <tonysabu@my.jcu.edu.au>
2016-05-30 11:38:35 +1000

    Second commit with new functions in itemlist.py.

--- itemlist.py
+++ itemlist.py
@@ -5,12 +5,31 @@ class ItemList:
     def __init__(self):
         self.items = []
 
-    def create_items(self, list_of_items):
-        for item in list_of_items:
-            self.items.append(Item(item[0], item[1], item[2], item[3]))
+    def __str__(self):
+        return "{}".format(self.items)
 
-    def get_items_list(self):
-        result = []
+    def create_list(self):
+
+        new_list = []
         for item in self.items:
-            result.append([item.name, item.description, item.status, item.price])
-            return result
+            name = item.name
+            description = item.description
+            price = item.price
+            status = item.status
+            new_list.append([name, description, price, status])
+        return new_list
+
+    def add_item(self, name, description, price, status):
+
+        self.items.append(Item(name, description, price, status))
+
+    def generate_list(self, item_list):
+
+        for item in item_list:
+            self.items.append(Item(item[0], item[1], float(item[2]), item[3]))
+
+    def get_item(self, name):
+
+        for item in self.items:
+            if item.name == name:
+                return item
