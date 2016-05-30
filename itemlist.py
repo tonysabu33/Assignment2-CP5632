c652e3c77fa93d9d5191fb842f4c81e61c2069b2
tonysabu33 <tonysabu@my.jcu.edu.au>
2016-05-30 15:01:09 +1000

    Final Commit with many changes to main.py and itemlist.py. Added the working save function to main.py. Only Small changes to app.kv Now the code is working good.

--- itemlist.py
+++ itemlist.py
@@ -2,15 +2,15 @@ from item import Item
 
 
 class ItemList:
-    def __init__(self):
+    def __init__(self):     # initializing ItemList
         self.items = []
 
     def __str__(self):
         return "{}".format(self.items)
 
-    def create_list(self):
+    def make_list(self):    # function to save items to the csv file at the closing of window
 
-        new_list = []
+        new_list = []       # creating empty list in a format ready to be written to .csv file
         for item in self.items:
             name = item.name
             description = item.description
@@ -21,7 +21,7 @@ class ItemList:
 
     def add_item(self, name, description, price, status):
 
-        self.items.append(Item(name, description, price, status))
+        self.items.append(Item(name, description, price, status))  # appending items to existing list
 
     def generate_list(self, item_list):
 
@@ -32,4 +32,4 @@ class ItemList:
 
         for item in self.items:
             if item.name == name:
-                return item
+                return item     # retrieving item from the item list
